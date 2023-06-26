6df0ff4fd2b2974be9ec8d2179a2eb188774e699
Stanley-deng <74106358+Stanley-deng@users.noreply.github.com>
2023-06-26 17:55:16 +0100

    update logging

--- core/clients/holostorage_accessor.py
+++ core/clients/holostorage_accessor.py
@@ -12,7 +12,9 @@ from os import path
 import requests
 
 from config import HOLOSTORAGE_ACCESSOR_HOST, HOLOSTORAGE_ACCESSOR_PORT
-from jobs.jobs_io import get_result_file_path_for_job
+    
+from jobs.jobs_io import (get_result_file_path_for_job, get_logger_for_job)
+
 
 # Note: Could be refactored such that more data is kept in job-specific temp
 # directories and less stuff has to be handed from component to component.
@@ -22,6 +24,8 @@ api_version = "v1"
 holograms_endpoint = f"{holostorage_baseurl}/api/{api_version}/holograms"
 
 def send_file_request_to_accessor(job_id: str, plid: str, medical_data: dict) -> None:
+    logger = get_logger_for_job(job_id)
+
     output_file_path = get_result_file_path_for_job(job_id)
     file_size_in_kb = int(path.getsize(output_file_path) / 1024)
 
@@ -38,7 +42,7 @@ def send_file_request_to_accessor(job_id: str, plid: str, medical_data: dict) ->
     response = requests.post(holograms_endpoint, data=request_body, files=files)
 
     if response.status_code == 200:
-        logging.info(f"Success! Created hologram: {response.json()}")
+        logger.info(f"Success! Created hologram: {response.json()}")
     else:
         raise Exception(f"Failed to created hologram: {response.json()}")
 
